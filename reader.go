032902783a370bf59612e7c5a4292c14ef772ac1
joshkestenberg <joshkestenberg@gmail.com>
2017-07-17 19:11:25 -0400

    added more err handling; refactored functions so they may stand alone

--- reader.go
+++ reader.go
@@ -19,147 +19,188 @@ type LogEntry struct {
 	Other   map[string]string `json:"other"`
 }
 
-//func to write json to file
-func writeJson(entry LogEntry, jsonLog *os.File) error {
-
-	jsonLogEntry, err := json.Marshal(entry)
-	if err != nil {
-		return err
-	}
-
-	_, err = jsonLog.WriteString(string(jsonLogEntry) + "\n")
-	if err != nil {
-		return err
-	}
-
-	return err
-}
-
-func openLog(filepath string) *os.File {
-	//open log file
+//open log file
+func OpenLog(filepath string) (*os.File, error) {
 	file, err := os.Open("./tendermint.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
+
+	return file, err
 }
 
-// set block bool to false in order to run non-block processing by default
-var block bool = false
+//generate and open json file
+func InitJson(filename string) (*os.File, error) {
+	err := ioutil.WriteFile(filename, nil, 0600)
 
-func main() {
-	//generate and open json file
-	err := ioutil.WriteFile("log.json", nil, 0600)
-	if err != nil {
-		log.Fatal(err)
+	jsonLog, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0600)
+
+	return jsonLog, err
+}
+
+func UnmarshalLines(startLn int, endLn int, file *os.File) ([]LogEntry, error) {
+	count := 1
+	block := false
+	var str string
+	var entry LogEntry
+	var err error
+	var entries []LogEntry
+
+	//basic error handling
+	if startLn < 1 || startLn > endLn {
+		err = fmt.Errorf("startLn must be greater than 1 and less than endLn; your input [startLn: %d, endLn: %d]", startLn, endLn)
 	}
-
-	jsonLog, err := os.OpenFile("log.json", os.O_RDWR|os.O_APPEND, 0600)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer jsonLog.Close()
-
-	file := openLog("./tendermint.log")
 
 	//intialize scanner and scan each line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 
-		//var for each full line of text
-		str := scanner.Text()
+		//var for full line of text
+		if count >= startLn && count <= endLn {
+			str = scanner.Text()
+		} else if count < startLn {
+			count++
+			continue
+		} else if count > endLn {
+			break
+		}
 
 		//skip over blank lines
 		if str == "" {
+			count++
 			continue
 		}
 
-		//verify we're not inside a block
-		if block == false {
+		//check if inside a block
+		if !strings.Contains(str, "|") {
+			block = true
+			count++
+			continue
+		} else if block == true && strings.Contains(str, "}#") && strings.Contains(str, "module") {
+			block = false
+			count++
+			continue
+		}
 
-			//init log struct
-			var entry LogEntry
+		//parse for level
+		levelParse := strings.SplitAfterN(str, "[", 2)
+		entry.Level = strings.Replace(levelParse[0], "[", "", 1)
 
-			//parse for level
-			levelParse := strings.SplitAfterN(str, "[", 2)
-			entry.Level = strings.Replace(levelParse[0], "[", "", 1)
+		//parse for date and time
+		datetimeParse := strings.SplitAfterN(levelParse[1], "]", 2)
 
-			//parse for date and time
-			datetimeParse := strings.SplitAfterN(levelParse[1], "]", 2)
+		entry.Date = strings.Split(datetimeParse[0], "|")[0]
+		entry.Time = strings.Replace(strings.Split(datetimeParse[0], "|")[1], "]", "", 1)
 
-			entry.Date = strings.Split(datetimeParse[0], "|")[0]
-			entry.Time = strings.Replace(strings.Split(datetimeParse[0], "|")[1], "]", "", 1)
+		//parse for descrip
+		descripParse := strings.Split(datetimeParse[1], "module")
+		descrip := descripParse[0]
 
-			//parse for descrip
-			descripParse := strings.Split(datetimeParse[1], "module")
-			descrip := descripParse[0]
-			//if the proceeding lines make up a block, we must process them differently by setting block to true
-			if strings.Contains(descrip, "Block{") {
-				entry.Module = "consensus"
+		//if the line describes a block, it needs to be specially processed
+		if strings.Contains(descrip, "Block{") {
+			entry.Module = "consensus"
 
-				if strings.Contains(descrip, "signed proposal block") {
-					entry.Descrip = "Signed proposal block"
-				} else {
-					entry.Descrip = "Block"
-				}
-				block = true
-
-				//write json
-				writeJson(entry, jsonLog)
-
-				continue
+			if strings.Contains(descrip, "signed proposal block") {
+				entry.Descrip = "Signed proposal block"
 			} else {
-				//set Descrip
-				entry.Descrip = strings.TrimSpace(descrip)
+				entry.Descrip = "Block"
 			}
-
-			//parse for all other entries
-			mapParse := strings.Split(descripParse[1], "=")[1:]
-			//set key to module for first iteration of loop (always module) and create empty map
-			key := "module"
-			m := make(map[string]string)
-			//iterate over other entries and add them to map
-			for _, element := range mapParse {
-				var valKey []string
-
-				if strings.Contains(element, `"`) {
-					valKey = strings.Split(element, `"`)[1:]
-				} else {
-					valKey = strings.SplitN(element, " ", 2)
-				}
-				fmt.Println(valKey)
-				//set Value
-				value := strings.TrimSpace(valKey[0])
-
-				if key == "module" && len(valKey) > 1 {
-					entry.Module = value
-					key = strings.TrimSpace(valKey[1])
-				} else if key == "module" {
-					entry.Module = value
-				} else if key != "module" && len(valKey) > 1 {
-					m[key] = value
-					key = strings.TrimSpace(valKey[1])
-				} else if key != "module" {
-					m[key] = value
-				}
-			}
-
-			// add map to entry struct
-			entry.Other = m
-
-			//write json
-			writeJson(entry, jsonLog)
+			entries = append(entries, entry)
+			count++
+			continue
 		} else {
-			// keep scanning and skipping lines until end of block
-			if strings.Contains(str, "}#") && strings.Contains(str, "module") {
-				block = false
-				continue
+			//set Descrip
+			entry.Descrip = strings.TrimSpace(descrip)
+		}
+
+		//parse for all other entries
+		mapParse := strings.Split(descripParse[1], "=")[1:]
+		//set key to module for first iteration of loop (always module) and create empty map
+		key := "module"
+		m := make(map[string]string)
+
+		//iterate over other entries and add them to map
+		for _, element := range mapParse {
+			var valKey []string
+
+			if strings.Contains(element, `"`) {
+				valKey = strings.Split(element, `"`)[1:]
+			} else {
+				valKey = strings.SplitN(element, " ", 2)
 			}
+
+			//set Value
+			value := strings.TrimSpace(valKey[0])
+
+			if key == "module" && len(valKey) > 1 {
+				entry.Module = value
+				key = strings.TrimSpace(valKey[1])
+			} else if key == "module" {
+				entry.Module = value
+			} else if key != "module" && len(valKey) > 1 {
+				m[key] = value
+				key = strings.TrimSpace(valKey[1])
+			} else if key != "module" {
+				m[key] = value
+			}
+		}
+
+		// add map to entry struct
+		entry.Other = m
+
+		count++
+		entries = append(entries, entry)
+		if count > endLn {
+			break
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	return entries, err
+}
+
+//func to write json to file
+func MarshalJson(entries []LogEntry, jsonLog *os.File) error {
+	var err error
+
+	for i := 0; i < len(entries); i++ {
+		jsonLogEntry, err := json.Marshal(entries[i])
+		if err != nil {
+			return err
+		}
+
+		_, err = jsonLog.WriteString(string(jsonLogEntry) + "\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+// set block bool to false in order to run non-block processing by default
+// var block bool = false
+
+func main() {
+
+	file, err := OpenLog("./tendermint.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonLog, err := InitJson("log.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	entries, err := UnmarshalLines(1, 50650, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = MarshalJson(entries, jsonLog)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
